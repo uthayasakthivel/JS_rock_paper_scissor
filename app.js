922b1f2b64ab5800c266befa85271e1e5dc7df6e
uthayasakthivel <uthayasakthivel.work@gmail.com>
2024-09-07 23:17:17 +0530

    project completed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const images = document.getElementsByClassName("image");
 const imagesArray = Array.from(images);
 console.log(imagesArray);
 const modal = document.getElementById("modal");
+const checkImages = document.getElementById("check_images");
 let selected = null;
 
 // click image
@@ -10,13 +11,15 @@ imagesArray.forEach((image) => {
     console.log(image.children[0].getAttribute("src"));
     const imageUrl = image.children[0].getAttribute("src");
     if (selected) {
-      modal.removeChild(selected);
+      checkImages.removeChild(selected);
     }
     selected = document.createElement("img");
     selected.setAttribute("src", imageUrl);
-    modal.append(selected);
+    checkImages.append(selected);
     console.log("selected", selected);
-    if ((modal.style.display = "none")) modal.style.display = "block";
+    if ((modal.style.display = "none")) {
+      modal.style.display = "block";
+    }
     generateRandomImage(selected);
   });
 });
@@ -30,12 +33,15 @@ function generateRandomImage(selected) {
   const randomImage = randomImageArray[randomNumber];
   const randomImagePic = document.createElement("img");
   randomImagePic.setAttribute("src", randomImage);
-  modal.append(randomImagePic);
+  checkImages.append(randomImagePic);
   const selectedImage = selected.getAttribute("src");
   const selectedText = selectedImage.split("/").pop().split(".").shift();
   const randomText = randomImage.split("/").pop().split(".").shift();
   const result = checkWin(selectedText, randomText);
   console.log(result);
+  const resultText = document.createElement("h3");
+  resultText.innerText = result;
+  checkImages.append(resultText);
 }
 
 function checkWin(selectedText, randomText) {
