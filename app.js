0ef71616cbc1277163cc1ccee8a02b493e67ae30
uthayasakthivel <uthayasakthivel.work@gmail.com>
2024-09-07 17:55:36 +0530

    get clicked image in the modal

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const images = document.getElementsByClassName("image");
 const imagesArray = Array.from(images);
-console.log(imagesArray);
+const modal = document.getElementById("modal");
+let selected = null;
+console.log(modal.querySelector("p"));
+
+// click image
 imagesArray.forEach((image) => {
   image.addEventListener("click", () => {
-    alert(image.children[1].innerText);
+    console.log(image.children[0].getAttribute("src"));
+    const imageUrl = image.children[0].getAttribute("src");
+    if (selected) {
+      modal.removeChild(selected);
+    }
+    selected = document.createElement("img");
+
+    selected.setAttribute("src", imageUrl);
+    modal.append(selected);
+
+    // selected.setAttribute("src", image.children[0]);
+    if ((modal.style.display = "none")) modal.style.display = "block";
   });
+});
+
+// Close
+modal.querySelector("p").addEventListener("click", () => {
+  if ((modal.style.display = "block")) modal.style.display = "none";
 });
